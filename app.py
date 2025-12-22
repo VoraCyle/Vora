04a7962a344c02960424b7236322591e596b4710
VoraCyle <aych92@gmail.com>
2025-12-21 23:01:40 -0600

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from rdkit.Chem import Descriptors
 
 # --- 1. SECURE CONFIGURATION ---
 if "GEMINI_API_KEY" in st.secrets:
-    genai.configure(api_key=st.secrets["AIzaSyDgQcX3wzpFaMnSZAKVj3O1B3izPuUpU2k"])
+    genai.configure(api_key=st.secrets["AIzaSyBEmthiZ4aKWUONVhvM4XAU9dCofarQ6EQ"])
     model = genai.GenerativeModel('gemini-1.5-flash') 
 else:
     st.error("🔑 API Key Missing. Please add GEMINI_API_KEY to your Streamlit Secrets.")
@@ -130,6 +130,7 @@ if query and query != "Select a problematic item...":
             
     else:
         st.error("Audit failed. Material signature not recognized.")
+
 
 
 
